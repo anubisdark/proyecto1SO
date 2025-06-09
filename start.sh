b56f471ed661e7e1fe943a36b35d574ab6f3dd3f
Emmanuel Antonio Pérez Ruiz <aeperez.prez4@gmail.com>
2025-06-09 10:46:02 -0400

    Add user and password for debian instance

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-docker-compose up -d --build
+#!/bin/bash
+
+# Limpiar contenedores anteriores
+docker compose down
+
+# Construir e iniciar
+docker compose up -d --build
+
+# Conectarse automáticamente
+ssh devcontainer
